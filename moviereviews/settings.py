0c6cf6e72eb0e87829444615a9de0da75116fe22
Daniel Correa <yo@danielgara.com>
2021-11-14 14:39:56 -0500

    imp: chapter 16

--- moviereviews/settings.py
+++ moviereviews/settings.py
@@ -129,3 +129,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    'moviereviews/static/',
+]
